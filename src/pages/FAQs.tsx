92930789042687021f54eafc5f3e644c10cb7468
unknown <ezekielabolade@gmail.com>
2022-12-16 00:27:02 +0100

    Responsiveness fixed

--- src/pages/FAQs.tsx
+++ src/pages/FAQs.tsx
@@ -9,7 +9,7 @@ function FAQs() {
         <Banner pageName="FAQ Page" page="faq"/>
         <div className="faqs">
             <h1>Frequently Asked Questions</h1>
-            <p className='text-[16px]'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
+            <p className='text-[16px] text-center'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the</p>
             <div className="questions-container">
                 {questions.questions.map(faq => <SingleFAQ question={faq.question} answer={faq.answer}/>)}
             </div>
